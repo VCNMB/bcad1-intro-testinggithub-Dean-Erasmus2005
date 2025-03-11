79a4b8fc899c1c03997024b4b95957b03e95677f
Dean-Erasmus2005 <ST10467060@vcconnect.edu.za>
2025-03-11 10:38:59 +0200

    added new class
    Added new class

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,10 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("Hello world I just did Github and hate Reece now");
+        
+        newClass nc = new newClass(); //creating a new class
+        nc.helloClass(); // have to use variable name nc to communcate with the class
+        
+        System.out.println(nc.strSecondClassVar);
     }
 }
